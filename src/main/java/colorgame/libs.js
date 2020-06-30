9ffe7ce373fb186bcdbaf21814bab4846014a39f
anson <zhang.yang6@husky.neu.edu>
2020-06-30 01:14:11 -0700

    Adds generateRandomColor method that randomly generates color.

--- src/main/java/colorgame/libs.js
+++ src/main/java/colorgame/libs.js
@@ -1,19 +1,11 @@
 <!--created by Anson zhang on June 30 2020.-->
 
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-]
-
+let colors = generateRandomColor(6);
 let squares = document.querySelectorAll(".square");
-
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -26,15 +18,39 @@ for (let i = 0; i < squares.length; i++) {
     // grab color of clicked square
     let clickedColor = this.style.background;
     // compare color to pickedColor
+    console.log(clickedColor,pickedColor);
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct!";
       // change each color to pickedColor
       changeColors(clickedColor);
+      h1.style.background = pickedColor;
     } else {
      this.style.background = "#232323";
      messageDisplay.textContent = "Try again!";
     }
   })
+}
+
+function generateRandomColor(number) {
+  // make an array of color
+  let array = [];
+  // repeat number times
+  for (let i = 0; i < number; i++) {
+    // get random color and push into array
+    array.push(randomColor());
+  }
+  // return that array
+  return array;
+}
+
+function randomColor() {
+  // pick a "red" from 0 -255
+  let red = Math.floor(Math.random() * 256);
+  // pick a "green" from 0 -255
+  let green = Math.floor(Math.random() * 256);
+  // pick a "blue" from 0 -255
+  let blue = Math.floor(Math.random() * 256);
+  return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
 function changeColors(color) {
